84dea3715de5523664acca1868222a71baf33f54
Gill <gillpreetsinghgill@gmail.com>
2022-08-07 20:44:59 -0700

    fixed buges of not available image

--- src/components/bookList/BookList.js
+++ src/components/bookList/BookList.js
@@ -2,18 +2,25 @@ import React from "react";
 import classes from "./BookList.module.css";
 
 function BookList({ booksData }) {
-  console.log(booksData);
   return (
     <div className={classes.container}>
       {booksData.map((book) => (
         <div key={book.id} className={classes.book}>
           <img
-            src={book.volumeInfo.imageLinks.thumbnail.replace(
-              "zoom-1",
-              "zoom-0"
-            )}
+            src={
+              book.volumeInfo.imageLinks
+                ? book.volumeInfo.imageLinks.thumbnail.replace(
+                    "zoom-1",
+                    "zoom-0"
+                  )
+                : "Image Not Available"
+            }
             alt="book-cover"
-            className={`${classes.book_img}`}
+            className={
+              book.volumeInfo.imageLinks
+                ? classes.bookImg
+                : classes.notAvailable
+            }
           />
         </div>
       ))}
